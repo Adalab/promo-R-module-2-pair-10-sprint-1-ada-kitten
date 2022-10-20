0e65d0bac5149595f6ffc141a5409423f36d4ea7
Elena <elenavargasm9@gmail.com>
2022-10-20 11:20:42 +0200

    update search description

--- js/main.js
+++ js/main.js
@@ -3,24 +3,25 @@
 const urlArticle1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 let name1 = "Anastacio";
 name1 = name1.toUpperCase();
-const description1 = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const description1 = "Risueño, cariñoso, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const race1 = "British Shorthair";
 
 const urlArticle2 = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
 let name2= "Fiona";
-const description2 = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+name2 = name2.toUpperCase();
+const description2 = "Risueño, cariñoso, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const race2 = "British Shorthair";
 
 const urlArticle3 = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
 let name3= "Cielo";
-const description3 = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+name3 = name3.toUpperCase();
+const description3 = "Risueño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const race3 = "British Shorthair";
 
 
 
 
-let form = document.querySelector(".js-new-form"); 
-form.classList.remove("collapsed");
+
 
 let list = document.querySelector(".js-list");
 let kitten1= `<li class="card">
@@ -64,4 +65,40 @@ let kitten3 = `<li class="card">
   </li>
 </article>`;
 
-list.innerHTML= kitten1 + kitten2 + kitten3;
+// list.innerHTML= kitten1 + kitten2 + kitten3;
+
+//Búsqueda por descripción
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+const descrSearchText = input_search_desc.value;
+if (description1.includes(descrSearchText)){
+  list.innerHTML += kitten1;
+}
+if (description2.includes(descrSearchText)){
+  list.innerHTML += kitten2;
+
+}
+if (description3.includes(descrSearchText)){
+  list.innerHTML += kitten3;
+}
+
+//Ocultar/ver formulario
+const form = document.querySelector(".js-new-form"); 
+
+if (form.classList.contains("collapsed")){
+  form.classList.remove("collapsed");
+}
+else{
+  form.classList.add("collapsed");
+}
+
+
+// bonus raza gatito
+
+let html = '';
+if (race1 === "") {
+  html = `No se ha especificado la raza`;
+} else {
+  html = race1;
+}
